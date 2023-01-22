2dc97355df8a45709183cc2453e00d69a0e8f38c
siptristan <sip.tristan@hotmail.com>
2023-01-22 14:16:57 +0700

    integratis admin email paid

--- app/Http/Controllers/Admin/CheckoutController.php
+++ app/Http/Controllers/Admin/CheckoutController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Checkout;
+use Mail;
+use App\Mail\Checkout\Paid;
 
 class CheckoutController extends Controller
 {
@@ -12,7 +14,11 @@ class CheckoutController extends Controller
     {
         $checkout->is_paid = true;
         $checkout->save();
+
+        // send email to user
+        Mail::to($checkout->User->email)->send(new Paid($checkout));
         $request->Session()->flash('success', "Checkout with ID {$checkout->id} has been updated!");
+
         return redirect(route('admin.dashboard'));
     }
 }
