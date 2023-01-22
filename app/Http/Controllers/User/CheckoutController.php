3387ce1e3a840775a749d8a75ed8652c942dce11
siptristan <sip.tristan@hotmail.com>
2023-01-22 13:49:46 +0700

    email integration, middleware admin dashboard, integration admin dashboard

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -6,8 +6,10 @@ use App\Http\Controllers\Controller;
 use App\Models\Checkout;
 use App\Models\Camp;
 use Illuminate\Http\Request;
-use Auth;
 use App\Http\Requests\User\Checkout\Store;
+use Auth;
+use Mail;
+use App\Mail\Checkout\AfterCheckout;
 
 class CheckoutController extends Controller
 {
@@ -30,7 +32,7 @@ class CheckoutController extends Controller
     {
         if ($camp->isRegistered) {
             $request->session()->flash('error', "You already registered on {$camp->title} camp.");
-            return redirect(route('dashboard'));
+            return redirect(route('user.dashboard'));
         }
         return view('checkout.create', ['camp' => $camp]);
     }
@@ -57,6 +59,9 @@ class CheckoutController extends Controller
 
         // create checkout
         $checkout = Checkout::create($data);
+
+        // sending email
+        Mail::to(Auth::user()->email)->send(new AfterCheckout($checkout));
 
         return redirect(route('checkout.success'));
     }
--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -26,4 +26,14 @@ class Checkout extends Model
     {
         return $this->belongsTo(Camp::class);
     }
+
+    /**
+     * Get the user that owns the Checkout
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
